d58160ae3effa340c37a24b3be3ff72993de8c71
EstelleJiani <jianiguoestelle@gmail.com>
2024-09-10 22:38:28 -0700

    Added a text in the Input component, showing the character count when user is typing characters; when the TextInput blurs, the counter turns invisible and indicate whether the inputed text is valid or not.

--- Components/Input.js
+++ Components/Input.js
@@ -5,18 +5,32 @@ import { useState } from 'react';
 
 export default function Input(props) {
   const [text, setText] = useState("");
+  const [showCounter, setShowCounter] = useState(true);
 
   return (
-    <TextInput
-    placeholder='Type something'
-    autoCorrect={true}
-    keyboardType='default'
-    value={text}
-    style={{borderBottomColor: "purple", borderBottomWidth: 2}}
-    onChangeText={(changedText) => {
-      setText(changedText)}}
-    autoFocus={props.autoFocus}
-    />
+    <View>
+        <TextInput
+            placeholder='Type something'
+            autoCorrect={true}
+            keyboardType='default'
+            value={text}
+            style={{borderBottomColor: "purple", borderBottomWidth: 2}}
+            onChangeText={(changedText) => {
+                setText(changedText)}
+                }
+            onBlur={() => {
+                setShowCounter(false)
+            }}
+            onFocus={() => {
+                setShowCounter(true)
+            }}
+            autoFocus={props.autoFocus}
+        />
+        <Text>{showCounter ? 
+            (text.length > 0 ? text.length : "") :
+            (text.length < 3 ?
+                "Please type more than 3 characters" : "Thank you")}</Text>
+    </View>
   )
 }
 
