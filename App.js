f7bfe52758294d22d110e8a188b3492f04b69c2f
EstelleJiani <jianiguoestelle@gmail.com>
2024-09-17 15:53:46 -0700

    Added the Confirm button, Modal, and updated the styling in the a new branch week3

--- App.js
+++ App.js
@@ -1,19 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View, SafeAreaView} from 'react-native';
 import Header from "./Components/Header";
 import Input from './Components/Input';
 import { useState } from 'react';
 
 export default function App() {
-  // define the app name
   const appName = 'My App';
+  const[receivedData, setReceivedData] = useState("");
+  const[modalVisible, setModalVisible] = useState(false);
+
+  function handleInputData(data){
+    console.log("App.js", data);
+    setReceivedData(data);
+    setModalVisible(false);
+  };
 
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="auto"/>
+
+      <View style={styles.border}>
       <Header name={appName}/>
-      <Input autoFocus={true}/>
-    </View>
+      </View>
+
+      <View style={styles.button}>
+      <Button 
+          title="Add a goal"
+          onPress={() => setModalVisible(true)}
+        />
+      </View>
+
+      <View style={styles.topView}>
+        <Input 
+          autoFocus={true} 
+          inputHandler={handleInputData} 
+          visibility={modalVisible}/>
+      </View>
+
+      <View style={styles.bottomView}>
+        <Text style={styles.text}>{receivedData}</Text>
+      </View>
+
+    </SafeAreaView>
   );
 }
 
@@ -23,5 +51,30 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text:{
+    color: 'white',
+    fontSize: 20,
+  },
+  topView:{
+    flex: 1,
+    bancgroundColor: 'lightblue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bottomView:{
+    flex: 4,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    width: "100%",
+  },
+  border:{
+    borderColor: 'black',
+    borderWidth: 2,
+  },
+  button:{
+    width:"30%",
+    margin:5,
+    backgroundColor:"grey",
   },
 });
--- Components/Input.js
+++ Components/Input.js
@@ -1,37 +1,62 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { TextInput, StyleSheet } from 'react-native'
-import { useState } from 'react';
+import { View, Text, Button, Modal, TextInput, StyleSheet} from 'react-native'
+import React, {useState} from 'react'
 
 export default function Input(props) {
   const [text, setText] = useState("");
   const [showCounter, setShowCounter] = useState(true);
+  
+  function handleConfirm() {
+    // console.log(text);
+    // call the callback function received from the App.js component
+    // and pass the text that user has typed
+    props.inputHandler(text);
+    }
 
-  return (
-    <View>
-        <TextInput
-            placeholder='Type something'
-            autoCorrect={true}
-            keyboardType='default'
-            value={text}
-            style={{borderBottomColor: "purple", borderBottomWidth: 2}}
-            onChangeText={(changedText) => {
-                setText(changedText)}
-                }
-            onBlur={() => {
-                setShowCounter(false)
-            }}
-            onFocus={() => {
-                setShowCounter(true)
-            }}
-            autoFocus={props.autoFocus}
-        />
-        <Text>{showCounter ? 
-            (text.length > 0 ? text.length : "") :
-            (text.length < 3 ?
-                "Please type more than 3 characters" : "Thank you")}</Text>
-    </View>
-  )
-}
+    return (
+        <Modal animationType='slide' visible={props.visibility}>
+        <View style={styles.container}>
+            <TextInput
+                style={styles.input}
+                autoFocus={props.autoFocus}
+                placeholder='Type something'
+                autoCorrect={true}
+                keyboardType='default'
+                value={text}
+                onChangeText={(changedText) => {
+                    setText(changedText)}
+                    }
+                onBlur={() => {
+                    setShowCounter(false)
+                }}
+                onFocus={() => {
+                    setShowCounter(true)
+                }}
+            />
 
-const styles = StyleSheet.create({})
+            <Text>{showCounter ? 
+                (text.length > 0 ? text.length : "") :
+                (text.length < 3 ?
+                    "Please type more than 3 characters" : "Thank you")}</Text>
+
+            <Button
+                title="Confirm"
+                onPress={handleConfirm}
+                style={styles.button}
+            />
+        </View>
+        </Modal>
+      )
+  }
+
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'white',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    input: {borderColor: 'purple',
+            borderWidth: 2,
+            padding: 10},
+  });
