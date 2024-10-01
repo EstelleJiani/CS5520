08dd1ae000b56c93efa984d66a150ab49c87d8ae
EstelleJiani <jianiguoestelle@gmail.com>
2024-10-01 13:29:45 -0700

    Added NavigationContainer into the App.js.

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Home from './Components/Home'
+import { NavigationContainer } from '@react-navigation/native';
 
 
 export default function App() {
-  return (<Home/>);
+  return (<NavigationContainer>
+    <Home/>
+  </NavigationContainer>
+  );
 }
